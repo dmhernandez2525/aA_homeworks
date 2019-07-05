272386f853420f82a21149b3b711f8de7a3dc69d
dmhernandez2525 <danielmark252525@gmail.com>
2019-07-05 09:53:40 -0500

    adds rspec homework

---
+++ w6/d4/Chess/display.rb
@@ -0,0 +1,16 @@
+require 'colorize'
+require 'colorized_string'
+require 'cursor.rb'
+
+class Display
+
+    def initialize(board)
+        @board = board
+        @cursor = Cursor.new([0,0], board)
+    end
+
+    def render
+
+    end
+
+end
